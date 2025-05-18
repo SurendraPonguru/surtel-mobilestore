b2e3d4fa639a735affb57ca0d60cb0ec55996665
Surendra-Ponguru <surendra.ponguru@kanerika.com>
2025-05-18 21:02:08 +0530

    theme is updated

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,7 +1,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light";
+type Theme = "light" | "dark";
 
 type ThemeContextType = {
   theme: Theme;
